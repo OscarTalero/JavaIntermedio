48d1e62f29406397be58148520c3e4b1051fbde7
Oscar Talero <talero@gmail.com>
2022-09-08 20:00:36 -0500

    S4 Exercise 1

---
+++ sesion456/session456/src/main/java/com/example/session456/Session456Application.java
@@ -0,0 +1,15 @@
+package com.example.session456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session456Application {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(Session456Application.class, args);
+	}
+
+}
